54255412c2dad532ff7fd7cffe35c4cf1ee93f77
Sanjay <sanjayyadav15.m@gmail.com>
2021-05-11 16:37:44 +0530

    modified Tester name in Extent report class

--- orangehrm/src/main/java/com/orangehrm/qa/utility/ExtentReportNG.java
+++ orangehrm/src/main/java/com/orangehrm/qa/utility/ExtentReportNG.java
@@ -17,7 +17,7 @@ public class ExtentReportNG {
 		
 		 extent =new ExtentReports();
 		extent.attachReporter(reporter);
-		extent.setSystemInfo("Tester", "Rahul Shetty");
+		extent.setSystemInfo("Tester", "Sanjay Yadav");
 		return extent;
 		
 	}
